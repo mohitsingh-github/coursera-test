20c1882623dbe4241046568e649c0861760d0ab2
mohitsingh-github <73215107+mohitsingh-github@users.noreply.github.com>
2020-11-09 12:42:25 +0530

    a

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -19,7 +19,7 @@ var allCategoriesUrl =
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl =
-  "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+  "menu_items.json?category=";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
 
